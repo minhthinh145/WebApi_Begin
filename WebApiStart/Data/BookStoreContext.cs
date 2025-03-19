a21759bca8933a5bd7b232bf39466be53a7cbfaf
minhthinh145 <minhthinh0384360149@gmail.com>
2025-03-19 14:01:26 +0700

    Init project web API

---
+++ WebApiStart/Data/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiStart.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+        #region Dbset
+        public DbSet<Book>? Books { get; set; }
+        #endregion
+    }
+}
